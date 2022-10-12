6560db2a7293f921f431c3ca8e80721b7fc192ba
Bodor István <istvanbodor19@gmail.com>
2022-10-12 15:02:17 +0200

    final

--- src/index.js
+++ src/index.js
@@ -15,3 +15,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 });*/
+document.addEventListener('DOMContentLoaded', () => {
+    var _a;
+    (_a = document.getElementById('felhasznalonev')) === null || _a === void 0 ? void 0 : _a.addEventListener('input', (e) => {
+        /*
+        let felh  = e.currentTarget as HTMLInputElement;
+        let felha : string = felh.value;
+        */
+        let felhasznalonev = document.getElementById('felhasznalonev').value;
+        if (felhasznalonev.length > 30 || felhasznalonev.length < 6) {
+            document.getElementById('userhiba').textContent = 'A felhasználónév 6-30 karakter hosszú kell legyen!';
+        }
+        else {
+            document.getElementById('userhiba').textContent = '';
+        }
+        if (felhasznalonev.includes('&') || felhasznalonev.includes('=') || felhasznalonev.includes('_') || felhasznalonev.includes('-') || felhasznalonev.includes('+') || felhasznalonev.includes(',') || felhasznalonev.includes("'") || felhasznalonev.includes('<') || felhasznalonev.includes('>') || felhasznalonev.includes('..')) {
+            document.getElementById('userhiba').textContent = 'A felhasználónév hibás karaktert tarzalmaz!';
+        }
+        else {
+            document.getElementById('userhiba').textContent = '';
+        }
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });*/
 
+document.addEventListener('DOMContentLoaded', () => {
+   
+        document.getElementById('felhasznalonev')?.addEventListener('input', (e) =>{
+                /*
+                let felh  = e.currentTarget as HTMLInputElement;
+                let felha : string = felh.value;
+                */
+               let felhasznalonev = (document.getElementById('felhasznalonev') as HTMLInputElement).value;
+              
+                if (felhasznalonev.length >30 || felhasznalonev.length< 6)
+                {
+                    (document.getElementById('userhiba') as HTMLElement).textContent = 'A felhasználónév 6-30 karakter hosszú kell legyen!';
+                } else {
+                    (document.getElementById('userhiba') as HTMLElement).textContent = '';
+                }
+
+                 if( felhasznalonev.includes('&') ||  felhasznalonev.includes('=') ||  felhasznalonev.includes('_') ||  felhasznalonev.includes('-') ||  felhasznalonev.includes('+') ||  felhasznalonev.includes(',') ||  felhasznalonev.includes("'") ||  felhasznalonev.includes('<') ||  felhasznalonev.includes('>') ||  felhasznalonev.includes('..'))
+                {
+                    (document.getElementById('userhiba') as HTMLElement).textContent = 'A felhasználónév hibás karaktert tarzalmaz!';
+                }
+                else {(document.getElementById('userhiba') as HTMLElement).textContent = '';}
+            }
+        );
+
+    
+
+
+
+});
